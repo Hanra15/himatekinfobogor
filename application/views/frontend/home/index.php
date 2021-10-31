5ed917eb5d9ab359efd5caedf4ea9f62b9788312
aliwafa16 <aliwafamuhamad16@gmail.com>
2021-10-31 04:53:09 -0700

    beres 4

--- application/views/frontend/home/index.php
+++ application/views/frontend/home/index.php
@@ -47,7 +47,7 @@
     </div>
 </section>
 
-<section class="profil">
+<section class="profil" style="background-color: #CEE5D0;">
     <div class="container pt-5 pb-5">
         <div class="row justify-content-center align-items-center">
             <div class="col-md-6">
@@ -62,21 +62,55 @@
 </section>
 
 
-<section class="coming_soon">
-
+<section class="coming_soon pb-3">
     <div class="container pt-3">
         <div class="row justify-content-center">
             <h1 class="blue">Coming</h1>
             <h1 class="purple">soon</h1>
         </div>
-
-        <div class="row justify-content-around">
-            <div class="col-md-6">
-                <h3>Detail Kegiatan</h3>
-            </div>
-            <div class="col-md-6">
-                <img src="<?= base_url() ?>assets/img/WEBINAR.jpg" style="max-height: 25rem;" alt="">
+        <div class="text-center">
+            <div class="row justify-content-center">
+                <div class="text-center">
+                    <img src="<?= base_url() ?>assets/img/IMG-HEADLINE.jpg" width="150px" alt="">
+                    <h3 class="purple">WEBINAR HIMATEKINFO</h3>
+                    <hr>
+                    <p class="text-center p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quasi harum aspernatur soluta tenetur nemo autem, vel, beatae deleniti vitae molestiae? Quaerat, incidunt. Quis quisquam fuga commodi, corporis explicabo expedita.</p>
+                </div>
+                <div class="col-md-3 m-2">
+                    <div class="card" style="width: 18rem; height:10rem">
+                        <div class="text-center">
+                            <img src="<?= base_url() ?>assets/img/idea.jpg" class="rounded mb-2 mt-3" width="75px" alt="">
+                            <h6 class="blue">Education Technology and Competition</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 m-2">
+                    <div class="card" style="width: 18rem; height:10rem">
+                        <div class="text-center">
+                            <img src="<?= base_url() ?>assets/img/map.png" class="rounded mb-2 mt-3" width="75px" alt="">
+                            <h6 class="blue">Fakultas Teknik dan Sains Universitas Ibn Khaldun Bogor</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 m-2">
+                    <div class="card" style="width: 18rem; height:10rem">
+                        <div class="text-center">
+                            <img src="<?= base_url() ?>assets/img/event.png" class="rounded mb-2 mt-3" width="75px" alt="">
+                            <h6 class="blue">9 s.d 14 Oktober 2021</h6>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        <div class="text-center mt-2">
+            <a href="#" class="btn" style="background-color: purple; color:white" type="button"><i class="fas fa-file-download"></i> Download Flayer</a>
+        </div>
+    </div>
+</section>
+
+<section class="post" style="background-color: #CEE5D0;">
+    <div class="container">
+        <h1 class="text-center">Post kegiatan</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto perspiciatis voluptatibus aliquid soluta doloremque cupiditate tenetur quidem ipsum eius voluptatem, blanditiis, distinctio dolore. Sed fuga quas ratione? Deleniti, laudantium sed voluptatum eum blanditiis facere. Voluptate, doloribus dicta, facere culpa fuga nemo iste hic aut perspiciatis, minus nam eos! Reiciendis, temporibus!</p>
     </div>
 </section>
